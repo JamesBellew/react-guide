edfa6073c21a99641cc36c77f00de34b2b25f82a
JamesBellew <47107576+JamesBellew@users.noreply.github.com>
2023-01-09 23:38:03 +0000

    Changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,53 @@
-import ExpenseItem from "./ExpenseItem";
+import { useState } from "react";
 import Card from "../UI/Card";
+import "../Expenses/Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
-      <h2>Let's get started!</h2>
-      <p>Let's get started!</p>
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    // const expenseFilter = "selected Filter";
+    console.log("In Expenses.js");
+    setFilteredYear(selectedYear);
+    // props.onAddExpense(expenseFilter);
+  };
+  const filteredExpenses = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      ></ExpenseItem>
-    </Card>
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+        <br></br>
+
+        {/*
+        <ExpenseItem
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+          date={props.item[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+          date={props.item[3].date}
+        ></ExpenseItem> */}
+      </Card>
+    </div>
   );
 };
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -44,7 +44,7 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expensetData);
+    props.onSaveExpenseData(expensetData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
